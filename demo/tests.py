950e04af64bc642088710e500ac0791627fd8fe7
Salvatore Tosti <saltosti@gmail.com>
2019-06-10 14:56:08 -0700

    Adds tests for API

--- demo/tests.py
+++ demo/tests.py
@@ -3,12 +3,15 @@ import unittest
 from pyramid import testing
 
 import transaction
+import json
 
 
 def dummy_request(dbsession):
     return testing.DummyRequest(dbsession=dbsession)
 
-
+def dummy_request_JSON_post(dbsession, json_params):
+    return testing.DummyRequest(dbsession=dbsession, json_body=json_params, method='POST')
+    
 class BaseTest(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp(settings={
@@ -39,28 +42,235 @@ class BaseTest(unittest.TestCase):
         transaction.abort()
         Base.metadata.drop_all(self.engine)
 
-
-class TestMyViewSuccessCondition(BaseTest):
-
+class TestInvoice(BaseTest):
     def setUp(self):
-        super(TestMyViewSuccessCondition, self).setUp()
+        super(TestInvoice, self).setUp()
         self.init_database()
 
-        from .models import MyModel
+    def test_invoice_view(self):
+        from .models import Invoice
+        from .views.invoice import invoice_view
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        response = invoice_view(dummy_request(self.session))
+        self.assertEqual(response['status'], 200)
+        
+    
+    def test_invoice_create_zero_date(self):
+        from .models import Invoice
+        from .views.invoice import invoice_create
+        data = {
+            'date': '0'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = invoice_create(request)
+        
+        self.assertEqual(response['status'], 200)
+        self.assertEqual(response['payload'], '{"id": 1, "date": "1969-12-31T16:00:00"}')
+    
+    def test_invoice_create_empty_date(self):
+        from .models import Invoice
+        from .views.invoice import invoice_create
+        data = {
+            'date' : ''
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = invoice_create(request)
 
-        model = MyModel(name='one', value=55)
-        self.session.add(model)
+        self.assertEqual(response['status'], 400)
+    
+    def test_invoice_create_no_date(self):
+        from .models import Invoice
+        from .views.invoice import invoice_create
+        data = {}
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = invoice_create(request)
 
-    def test_passing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'demo')
+        self.assertEqual(response['status'], 200)
 
+class TestInvoiceItem(BaseTest):
+    def setUp(self):
+        super(TestInvoiceItem, self).setUp()
+        self.init_database()
 
-class TestMyViewFailureCondition(BaseTest):
+    def test_item_create_valid(self):
+        from .models import Invoice
+        from .views.item import item_create
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        data = {
+            'units':'1',
+            'amount':'1.00',
+            'description' : 'test',
+            'parent_id' : '1'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = item_create(request)
+        
+        self.assertEqual(response['status'], 200)
+    
+    def test_item_create_invalid_units(self):
+        from .models import Invoice
+        from .views.item import item_create
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        data = {
+            'units':'fdfas',
+            'amount':'1.00',
+            'description' : 'test',
+            'parent_id' : '1'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = item_create(request)
+        
+        self.assertEqual(response['status'], 400)
+    
+    def test_item_create_negative_units(self):
+        from .models import Invoice
+        from .views.item import item_create
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        data = {
+            'units':'-1',
+            'amount':'1.00',
+            'description' : 'test',
+            'parent_id' : '1'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = item_create(request)
+        
+        self.assertEqual(response['status'], 400)
+    
+    def test_item_create_invalid_amount(self):
+        from .models import Invoice
+        from .views.item import item_create
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        data = {
+            'units':'1',
+            'amount':'-1.00',
+            'description' : 'test',
+            'parent_id' : '1'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = item_create(request)
+        
+        self.assertEqual(response['status'], 400)
+    
+    def test_item_create_negative_parent_id(self):
+        from .models import Invoice
+        from .views.item import item_create
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        data = {
+            'units':'1',
+            'amount':'1.00',
+            'description' : 'test',
+            'parent_id' : '-1'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = item_create(request)
+        
+        self.assertEqual(response['status'], 400)
+    
+    def test_item_create_invalid_parent_id(self):
+        from .models import Invoice
+        from .views.item import item_create
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        data = {
+            'units':'1',
+            'amount':'1.00',
+            'description' : 'test',
+            'parent_id' : '99'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = item_create(request)
+        
+        self.assertEqual(response['status'], 400)
+    
+    def test_item_create_missing_units(self):
+        from .models import Invoice
+        from .views.item import item_create
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        data = {
+            'amount':'1.00',
+            'description' : 'test',
+            'parent_id' : '1'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = item_create(request)
+        
+        self.assertEqual(response['status'], 400)
+    
+    def test_item_create_missing_amount(self):
+        from .models import Invoice
+        from .views.item import item_create
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        data = {
+            'units':'1',
+            'description' : 'test',
+            'parent_id' : '1'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = item_create(request)
+        
+        self.assertEqual(response['status'], 400)
+        
+    def test_item_create_missing_description(self):
+        from .models import Invoice
+        from .views.item import item_create
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        data = {
+            'units':'1',
+            'amount':'1.00',
+            'parent_id' : '1'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = item_create(request)
+        
+        self.assertEqual(response['status'], 400)
 
-    def test_failing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info.status_int, 500)
+    def test_item_create_missing_parent_id(self):
+        from .models import Invoice
+        from .views.item import item_create
+        
+        invoice = Invoice()
+        self.session.add(invoice)
+        data = {
+            'units':'1',
+            'amount':'1.00',
+            'description' : 'test'
+        }
+        
+        request = dummy_request_JSON_post(self.session, data)     
+        response = item_create(request)
+        
+        self.assertEqual(response['status'], 400)
+    
+    
